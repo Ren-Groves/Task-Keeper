d0680f2bab71b10bedfed5a5c6717366e941c851
Ren-Groves <grovesc05@uww.edu>
2024-12-04 11:15:57 -0600

    add buttons

--- sketch.js
+++ sketch.js
@@ -4,4 +4,22 @@ function setup() {
 
 function draw() {
   background(255);
+  fillGradient('linear', {
+    from : [0, 5],
+    to : [500, 35],
+    steps : [
+      color(148, 0, 0),
+      color(255, 0, 0),
+      color(255, 145, 0),
+      color(255, 222, 0),
+      color(196, 255, 0),
+      color(84, 210, 0),
+    ]
+  });
+  rect(10, 5, 80, 30);
+  rect(110, 5, 80, 30);
+  rect(10, 5, 80, 30);
+  rect(210, 5, 80, 30);
+  rect(310, 5, 80, 30);
+  rect(410, 5, 80, 30);
 }
