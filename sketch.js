e68ab117ff55582cb3ff6072797bc4ba7eb2d1a7
Ren-Groves <grovesc05@uww.edu>
2024-12-04 11:25:00 -0600

    make the buttons work

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 function setup() {
   createCanvas(500, 500);
+  background(255);
 }
 
 function draw() {
-  background(255);
   fillGradient('linear', {
     from : [0, 5],
     to : [500, 35],
@@ -22,4 +22,32 @@ function draw() {
   rect(210, 5, 80, 30);
   rect(310, 5, 80, 30);
   rect(410, 5, 80, 30);
+}
+
+function mouseClicked() {
+  if (mouseX < 90 && mouseY < 35) {
+    fill(0)
+    textSize(15);
+    text('5', 250, 250);
+  }
+  if (mouseX < 190 && mouseX > 90 && mouseY < 35) {
+    fill(0)
+    textSize(15);
+    text('4', 250, 250);
+  }
+  if (mouseX < 290 && mouseX > 190 && mouseY < 35) {
+    fill(0)
+    textSize(15);
+    text('3', 250, 250);
+  }
+  if (mouseX < 390 && mouseX > 290 && mouseY < 35) {
+    fill(0)
+    textSize(15);
+    text('2', 250, 250);
+  }
+  if (mouseX < 490 && mouseX > 390 && mouseY < 35) {
+    fill(0)
+    textSize(15);
+    text('1', 250, 250);
+  }
 }
